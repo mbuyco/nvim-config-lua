3a1d40a11731e3ecab80047862ae6db5246d81d2
Mike Buyco <mbuyco@foodbyus.com>
2023-04-09 11:25:40 +0800

    update lspconfig keymap

--- plugin/nvim-lspconfig.lua
+++ plugin/nvim-lspconfig.lua
@@ -34,7 +34,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gd', '<C-w>]<CR>', opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
