c435c56b9ad4f8e79f927add106c64616f50b076
Mike Buyco <mbuyco@foodbyus.com>
2023-05-11 07:35:48 +0800

    update telescope config

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -11,25 +11,22 @@ telescope.setup {}
 telescope.load_extension('fzf')
 telescope.load_extension('fzy_native')
 
-function find_files_from_project_git_root()
+-- Falling back to find_files if git_files can't find a .git directory
+function project_files()
   local function is_git_repo()
     vim.fn.system("git rev-parse --is-inside-work-tree")
     return vim.v.shell_error == 0
   end
-  local function get_git_root()
-    local dot_git_path = vim.fn.finddir(".git", ".;")
-    return vim.fn.fnamemodify(dot_git_path, ":h")
-  end
   local opts = {}
   if is_git_repo() then
-    opts = {
-      cwd = get_git_root(),
-    }
+    require("telescope.builtin").git_files({})
+  else
+    require("telescope.builtin").find_files({})
   end
-  require("telescope.builtin").find_files(opts)
 end
 
-function live_grep_from_project_git_root()
+-- Live grep from project git root with fallback
+function live_grep()
 	local function is_git_repo()
 		vim.fn.system("git rev-parse --is-inside-work-tree")
 
@@ -54,7 +51,7 @@ end
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', find_files_from_project_git_root, {})
-vim.keymap.set('n', '<leader>fg', live_grep_from_project_git_root, {})
+vim.keymap.set('n', '<leader>ff', project_files, {})
+vim.keymap.set('n', '<leader>fg', live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
