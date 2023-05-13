ab34852634bb4f149bbf87d808e03cb8b7bda914
Mike Buyco <mbuyco@foodbyus.com>
2023-05-14 01:29:38 +0800

    update live_grep to use git dir

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -34,33 +34,19 @@ function project_files()
   end
 end
 
--- Live grep from project git root with fallback
-function live_grep()
-	local function is_git_repo()
-		vim.fn.system("git rev-parse --is-inside-work-tree")
-
-		return vim.v.shell_error == 0
-	end
-
-	local function get_git_root()
-		local dot_git_path = vim.fn.finddir(".git", ".;")
-		return vim.fn.fnamemodify(dot_git_path, ":h")
-	end
-
-	local opts = {}
-
-	if is_git_repo() then
-		opts = {
-			cwd = get_git_root(),
-		}
-	end
-
-	require("telescope.builtin").live_grep(opts)
+-- Live grep from project git root
+function live_grep_git_dir()
+  local git_dir = vim.fn.system(string.format("git -C %s rev-parse --show-toplevel", vim.fn.expand("%:p:h")))
+  git_dir = string.gsub(git_dir, "\n", "") -- remove newline character from git_dir
+  local opts = {
+    cwd = git_dir,
+  }
+  require('telescope.builtin').live_grep(opts)
 end
 
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', project_files, {})
-vim.keymap.set('n', '<leader>fg', live_grep, {})
+vim.keymap.set('n', '<leader>fg', live_grep_git_dir, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
