359cce77a807b0ac60a897a31fb68be9d7c1c250
Mike Buyco <mbuyco@foodbyus.com>
2023-07-24 07:30:57 +0800

    update nvim config

--- plugin/php-cs-fixer.vim
+++
@@ -1,3 +0,0 @@
-let g:php_cs_fixer_path="~/src/dev/tools/php-cs-fixer/vendor/bin/php-cs-fixer"
-
-autocmd BufWritePost *.php silent! call PhpCsFixerFixFile()
--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -3,6 +3,9 @@ if not status_ok then
 	return
 end
 
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 telescope.setup {
@@ -12,6 +15,13 @@ telescope.setup {
       '--vimgrep',
       '--smart-case',
       '-j1'
+    },
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key",
+        ["<C-j>"] = "cycle_history_next",
+        ["<C-k>"] = "cycle_history_prev"
+      }
     }
   }
 }
@@ -20,20 +30,6 @@ telescope.setup {
 telescope.load_extension('fzf')
 telescope.load_extension('fzy_native')
 
--- Falling back to find_files if git_files can't find a .git directory
-function project_files()
-  local function is_git_repo()
-    vim.fn.system("git rev-parse --is-inside-work-tree")
-    return vim.v.shell_error == 0
-  end
-  local opts = {}
-  if is_git_repo() then
-    require("telescope.builtin").git_files({})
-  else
-    require("telescope.builtin").find_files({})
-  end
-end
-
 -- Live grep from project git root
 function live_grep_git_dir()
   local git_dir = vim.fn.system(string.format("git -C %s rev-parse --show-toplevel", vim.fn.expand("%:p:h")))
@@ -41,12 +37,10 @@ function live_grep_git_dir()
   local opts = {
     cwd = git_dir,
   }
-  require('telescope.builtin').live_grep(opts)
+  builtin.live_grep(opts)
 end
 
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>ff', project_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', live_grep_git_dir, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
