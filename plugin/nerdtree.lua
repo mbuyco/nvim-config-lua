db2faca16403daecb625f5f3791fe68f8171f5b1
Mike Buyco <mbuyco@foodbyus.com>
2023-10-10 11:54:04 +0800

    update show in folder

--- plugin/nerdtree.lua
+++ plugin/nerdtree.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>f', ':NERDTreeFind<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>F', ':NERDTreeFind<CR>', { noremap = true })
