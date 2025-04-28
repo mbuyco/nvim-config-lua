92db86399d06da3843fb20481b33e3662e1581e5
Mike <mbuyco@protonmail.com>
2025-04-28 16:41:50 +0800

    add css, html, scss and typescript to treesitter configs

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,16 +1,20 @@
 require('utils').setup_config('nvim-treesitter.configs', {
   ensure_installed = {
     "bash",
+    "css",
     "go",
+    "html",
     "javascript",
     "json",
     "lua",
     "php",
     "python",
     "ruby",
+    "scss",
+    "typescript",
     "vim",
-    "yaml",
     "vue",
+    "yaml",
   }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
