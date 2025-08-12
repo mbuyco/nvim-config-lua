8ce1f0f540f52b07f161a5a0859e896ea257855f
malayalabs <mbuyco@protonmail.com>
2025-08-12 22:35:47 +1000

    update lazy-lock.json

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -110,7 +110,9 @@ return {
 
   {
     "iamcco/markdown-preview.nvim",
-    config = function() vim.fn["mkdp#util#install"]() end,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
   },
 
   {
